2bcdbd90248cfc195b777286dc71434808065def
Anders Thøgersen <anders@bladre.dk>
2016-02-21 16:34:09 +0100

    add missing cemerick.piggieback - prevent "analysis error"
    Seems to prevent these errors:
    
        $ lein figwheel
        Figwheel: Starting server at http://localhost:3449
        Figwheel: Watching build - dev
        Compiling "resources/public/index.js" from  ["src/"]...
        {:tag :cljs/analysis-error}
        ANALYSIS ERROR: No reader function for tag object on file null, line null,
        column null
        Successfully compiled "resources/public/index.js" in 1.302 seconds.
    						    Figwheel: Starting nREPL server
        on port: 7888
        WARNING: unable to load "cemerick.piggieback/wrap-cljs-repl" middleware
    								Launching
        ClojureScript REPL for build: dev
        Figwheel Controls:

--- project.clj
+++ project.clj
@@ -26,6 +26,12 @@
    "figwheel_server.log"
    "target/"]
 
+  :profiles
+  {:dev  {:dependencies  [[com.cemerick/piggieback "0.2.1"]
+                          [org.clojure/tools.nrepl "0.2.10"]]
+          :repl-options  {:nrepl-middleware
+                          [cemerick.piggieback/wrap-cljs-repl]}}}
+
   :cljsbuild 
   {:builds 
    [{:id "dev"
