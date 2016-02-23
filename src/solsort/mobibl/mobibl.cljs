4696395d658396a414080212b192304a6e17f10d
Anders Thøgersen <anders@bladre.dk>
2016-02-23 09:52:10 +0100

    remove conflict marker

--- src/solsort/mobibl/mobibl.cljs
+++ src/solsort/mobibl/mobibl.cljs
@@ -54,7 +54,6 @@
     [cljs.core.async.macros :refer [go go-loop alt!]]
     [reagent.ratom :as ratom :refer  [reaction]])
   (:require
-<<<<<<< HEAD
     [solsort.util
      :refer
      [<ajax <seq<! js-seq normalize-css load-style! put!close! parse-json-or-nil log page-ready render
