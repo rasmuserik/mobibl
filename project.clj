5be5dbed049bb65c9ca9670e48e3fed5464f64fd
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-03-01 13:15:01 +0100

    cleanup project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-(def server-port (read-string (or (System/getenv "FIGWHEEL_SERVER_PORT") "3449")))
-(def nrepl-port (read-string (or (System/getenv "FIGWHEEL_NREPL_PORT") "7888")))
 (defproject solsort.mobibl/mobibl "0.0.1-SNAPSHOT"
   :license "Creative Commons BY-NC-ND"
 
@@ -10,21 +8,20 @@
    [cljsjs/pouchdb "5.2.1-0"]
    [cljsjs/showdown "0.4.0-1"]
    [solsort/util "0.1.2-SNAPSHOT"]
+   ; maybe use [secretary "1.2.3"]
    [reagent "0.5.1"]
-   [re-frame "0.6.0"]
-   [soda-ash "0.1.0-beta"]]
+   [re-frame "0.6.0"]]
 
   :plugins
   [[lein-cljsbuild "1.1.1"]
    [lein-ancient "0.6.8"]
    [lein-figwheel "0.5.0-2"]
    [lein-bikeshed "0.2.0"]
-   [lein-kibit "0.1.2"]
-   ]
+   [lein-kibit "0.1.2"]]
 
   :source-paths ["src/" "test/"]
 
-  :clean-targets ^{:protect false} 
+  :clean-targets ^{:protect false}
   ["resources/public/out"
    "resources/public/index.js"
    "resources/public/tests.js"
@@ -36,35 +33,43 @@
   ;:doo {:build "test"}
 
   :profiles
-  {:dev  {:dependencies  [[com.cemerick/piggieback "0.2.1"]
-                          [org.clojure/tools.nrepl "0.2.10"]]
-          :repl-options  {:nrepl-middleware
-                          [cemerick.piggieback/wrap-cljs-repl]}}}
+  {:dev
+   {:dependencies
+    [[com.cemerick/piggieback "0.2.1"]
+     [org.clojure/tools.nrepl "0.2.10"]]
+    :repl-options
+    {:nrepl-middleware
+     [cemerick.piggieback/wrap-cljs-repl]}}}
 
-  :cljsbuild 
-  {:builds 
+  :cljsbuild
+  {:builds
    [{:id "dev"
      :source-paths ["src/"]
-     :figwheel {:websocket-host ~(.getHostAddress (java.net.InetAddress/getLocalHost))
-               ; :on-jsload "" 
-                }
-     :compiler {:main solsort.mobibl.html5
-                :asset-path "out"
-                :output-to "resources/public/index.js"
-                :output-dir "resources/public/out"
-                :source-map-timestamp true }}
+     :figwheel
+     {:websocket-host ~(.getHostAddress (java.net.InetAddress/getLocalHost))
+      ; :on-jsload ""
+      }
+     :compiler
+     {:main solsort.mobibl.html5
+      :asset-path "out"
+      :output-to "resources/public/index.js"
+      :output-dir "resources/public/out"
+      :source-map-timestamp true }}
     {:id "test"
      :source-paths ["src" "test"]
-     :compiler {:output-to "out/testable.js"
-                :main 'solsort.mobibl.runner
-                :source-map true
-                :optimizations :none}}
+     :compiler
+     {:output-to "out/testable.js"
+      :main 'solsort.mobibl.runner
+      :source-map true
+      :optimizations :none}}
     {:id "dist"
      :source-paths ["src"]
-     :compiler {:output-to "index.js"
-                :main solsort.mobibl.html5
-                :externs ["externs.js"]
-                :optimizations :advanced
-                :pretty-print false}}]}
-                :figwheel {:nrepl-port ~nrepl-port
-                            :server-port ~server-port})
+     :compiler
+     {:output-to "index.js"
+      :main solsort.mobibl.html5
+      :externs ["externs.js"]
+      :optimizations :advanced
+      :pretty-print false}}]}
+  :figwheel
+  {:nrepl-port ~(read-string (or (System/getenv "FIGWHEEL_SERVER_PORT") "3449"))
+   :server-port ~(read-string (or (System/getenv "FIGWHEEL_NREPL_PORT") "7888"))  })
