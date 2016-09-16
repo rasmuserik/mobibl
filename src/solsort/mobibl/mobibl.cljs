c6d1a4a3df73c11bf3ef7b8e50d35177bb29a544
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-09-16 14:33:01 +0200

    revert previous commit, apparently it wasn't needed

--- src/solsort/mobibl/mobibl.cljs
+++ src/solsort/mobibl/mobibl.cljs
@@ -432,13 +432,13 @@
     [:div.ui.fluid.labeled.input
      [:div.ui.label "Lånernummer"]
      [input {:type "tel"
-             ;:style {"-webkit-text-security" "disc"}
+             :style {"-webkit-text-security" "disc"}
              :db [:login :user]}]]]
    [:p
     [:div.ui.fluid.labeled.input
      [:div.ui.label "Pin"]
      [input {:type "tel"
-             ;:style {"-webkit-text-security" "disc"}
+             :style {"-webkit-text-security" "disc"}
              :db [:login :pin]}]]]
    [:p {:style {:text-align :right}}
     [:span.primary.ui.button
